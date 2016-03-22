2200ca79c5dd0677bf21b1f323bea74be61a9020
ClassCat <support@classcat.com>
2016-03-22 15:14:52 +0900

    rc 0xff : kernel build

--- scripts_for_kernel_build/build_kernel.sh
+++ scripts_for_kernel_build/build_kernel.sh
@@ -5,6 +5,7 @@
 ###################################################################
 
 # --- HISTORY -----------------------------------------------------
+# 22-mar-16 : rc 0xff.
 # 03-mar-16 : changed the url.
 # 29-feb-16 : created.
 # -----------------------------------------------------------------
@@ -12,6 +13,22 @@
 
 . ../conf_for_kernel_build/build.conf
 
+
+function check_if_continue () {
+  local var_continue
+
+  echo -ne "About to build kernel. Continue ? (y/n) : " >&2
+
+  read var_continue
+  if [ -z "$var_continue" ] || [ "$var_continue" != 'y' ]; then
+    echo -e "Exit the install program."
+    echo -e ""
+    exit 1
+  fi
+}
+
+
+check_if_continue
 
 apt-get install -y build-essential libncurses5-dev
 
@@ -27,11 +44,31 @@ cp -p /boot/config-3.13.0-${KERNEL_REVISION}-generic ./.config
 
 time make -j ${NUMBER_OF_CORES}
 
+if [ "$?" != 0 ]; then
+  echo "Script aborted. make -j ${NUMBER_OF_CORES} failed."
+  exit 1
+fi
+
+
+
 ###
 ### Store it to s3.
 ###
 
 s3cmd put drivers/gpu/drm/drm.ko s3://cctf-classcat-com/ubuntu-kernel/drm.ko.3.13.0-${KERNEL_REVISION}-generic
+
+if [ "$?" != 0 ]; then
+  echo "Script aborted. s3cmd put failed."
+  exit 1
+fi
+
+
+###
+
+echo ""
+echo "Done."
+echo ""
+
 
 
 ### End of Script ###
--- scripts_for_kernel_build/init_instance.sh
+++ scripts_for_kernel_build/init_instance.sh
@@ -5,6 +5,7 @@
 ###################################################################
 
 # --- HISTORY -----------------------------------------------------
+# 22-mar-16 : rc 0xff.
 # 01-mar-16 : use build_kernel.conf instead of init_instance.conf
 # 01-mar-16 : msg out.
 # 29-feb-16 : created.
@@ -14,23 +15,40 @@
 . ../conf_for_kernel_build/build.conf
 
 
-apt-get update
+function check_if_continue () {
+  local var_continue
 
-apt-get -y upgrade
+  echo -ne "About to init an instance for kernel build. Continue ? (y/n) : " >&2
 
-apt-get -y dist-upgrade
+  read var_continue
+  if [ -z "$var_continue" ] || [ "$var_continue" != 'y' ]; then
+    echo -e "Exit the install program."
+    echo -e ""
+    exit 1
+  fi
+}
 
-apt-get install -y ntp
+
+function init_instance() {
+
+  ### Basic ###
+  apt-get update
+  apt-get -y upgrade
+  apt-get -y dist-upgrade
+
+  apt-get install -y ntp
+
+  ### /etc/hosts to disable ipv6 ###
+  cp -p /etc/hosts /etc/hosts.orig
+  echo "127.0.0.1\tlocalhost\n" > /etc/hosts
+
+  ### BACKUP ###
+  cp -a /etc /etc.ec2.orig
 
 
-### BACKUP ###
+  ### SWAP ###
 
-cp -a /etc /etc.ec2.orig
-
-
-### SWAP ###
-
-cp -p /etc/rc.local /etc/rc.local.orig
+  cp -p /etc/rc.local /etc/rc.local.orig
 
 cat <<_EOB_ > /etc/rc.local
 #!/bin/sh -e
@@ -54,15 +72,43 @@ fallocate -l \$SIZE \$SWAPFILENAME && mkswap \$SWAPFILENAME && swapon \$SWAPFILE
 exit 0
 _EOB_
 
+}
 
-### S3CMD ###
 
-apt-get install s3cmd
+###
+### INIT
+###
 
-install -o root -g root ../assets/s3cfg.north-east-1 /root/.s3cfg
+function init () {
+  check_if_continue
 
-sed -i.tmpl -e "s/^access_key\s*=.*/access_key = ${S3CMD_ACCESS_KEY}/g" /root/.s3cfg
-sed -i      -e "s/^secret_key\s*=.*/secret_key = ${S3CMD_SECRET_KEY}/g" /root/.s3cfg
+  init_instance
+}
+
+
+
+###
+### S3CMD
+###
+
+function install_and_config_s3cmd () {
+  apt-get install -y s3cmd
+
+  install -o root -g root ../assets/s3cfg.north-east-1 /root/.s3cfg
+
+  sed -i.tmpl -e "s/^access_key\s*=.*/access_key = ${S3CMD_ACCESS_KEY}/g" /root/.s3cfg
+  sed -i      -e "s/^secret_key\s*=.*/secret_key = ${S3CMD_SECRET_KEY}/g" /root/.s3cfg
+
+}
+
+
+###################
+### ENTRY POINT ###
+###################
+
+init
+
+install_and_config_s3cmd
 
 
 ### 
